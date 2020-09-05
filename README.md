95db2d848683e650205674e92e6eb01723af9a97
osmangandon <osmangandon@gmail.com>
2020-09-05 21:14:56 +0700

    2.0

---
+++ README.md
@@ -0,0 +1,66 @@
+# sisfokol_presensigps
+
+WEB PRESENSI GPS.
+
+Gunakan WebServer XAMPP PHP 7.4.8
+
+
+
+Pastikan server komputer dengan IP 192.168.1.100
+
+dan folder web berada di folder "sisfokol_presensigps".
+
+sehingga bisa diakses dari komputer lain dengan alamat http://192.168.1.100/sisfokol_presensigps
+
+
+
+konfigurasi 
+---
+
+1. Buka file /inc/config.php lakukan setting koneksi database dan path server dengan benar.
+
+2. Jalankan PhpMyAdmin, buatlah sebuah database. Kemudian import file .sql dari folder /db
+
+3. Web PresensiGPS bisa diakses dengan alamat http://192.168.1.100/sisfokol_presensigps
+
+4. login admin, user : admin , pass : Sisfokol2020
+
+
+
+
+NB.
+---
+
+Pastikan koneksi internet aktif. Agar peta bisa terlihat.
+
+
+
+
+APK Android sesuai setting yang ada :
+---
+
+"sisfokol_presensigps.apk" ada di folder filebox
+
+
+
+
+
+
+Untuk android, source code ada di :
+---
+
+https://github.com/hajirodeon/sisfokol_android_presensigps
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ adm/s/i_alamat.php
@@ -0,0 +1,64 @@
+<?php
+require("../../inc/config.php");
+require("../../inc/fungsi.php");
+require("../../inc/koneksi.php");
+
+
+
+$id_provinsi  = cegah($_POST['id_provinsi']);
+$id_kabupaten  = cegah($_POST['id_kabupaten']);
+
+
+if(isset($_POST["id_provinsi"]) && !empty($_POST["id_provinsi"]))
+	{
+	$qku = mysqli_query($koneksi, "SELECT * FROM kabupaten ".
+							"WHERE id_prov = '$id_provinsi' ".
+  							"ORDER BY nama ASC");
+	$rku = mysqli_fetch_assoc($qku);
+
+	do
+		{
+		$ku_idkab = nosql($rku['id_kab']);
+		$ku_nama = balikin($rku['nama']);
+		$ku_nama2 = cegah($rku['nama']);
+		
+
+		echo '<option value="'.$ku_idkab.'">'.$ku_nama.'</option>';
+		}
+	while ($rku = mysqli_fetch_assoc($qku));
+	
+	exit();
+  	}
+
+
+
+
+if(isset($_POST["id_kabupaten"]) && !empty($_POST["id_kabupaten"]))
+	{
+	$qku = mysqli_query($koneksi, "SELECT * FROM kecamatan ".
+							"WHERE id_kab = '$id_kabupaten' ".
+  							"ORDER BY nama ASC");
+	$rku = mysqli_fetch_assoc($qku);
+
+	do
+		{
+		$ku_idkec = nosql($rku['id_kec']);
+		$ku_nama = balikin($rku['nama']);
+		$ku_nama2 = cegah($rku['nama']);
+
+		echo '<option value="'.$ku_idkec.'">'.$ku_nama.'</option>';
+		}
+	while ($rku = mysqli_fetch_assoc($qku));
+	
+	exit();
+  	}
+
+
+
+
+
+
+
+
+exit();
+?>
